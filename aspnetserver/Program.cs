21fe2b6925a5cef1fda9f126aa776893cb27b209
Michael VanderLoon <49925095+TNT928@users.noreply.github.com>
2023-07-11 10:56:20 -0400

    small change to program.cs

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-             .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+             .WithOrigins("http://localhost:3000", "https://witty-ground-09d843e10.3.azurestaticapps.net");
         });
 });
 
